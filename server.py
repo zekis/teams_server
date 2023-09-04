729f1d534d35d8f58415075189cd561307038842
Zeke Tierney <zeke.tierney@gmail.com>
2023-09-04 23:00:21 +0800

    working admin and custom page

--- server.py
+++ server.py
@@ -24,12 +24,32 @@ from aiohttp import web
 
 from botbuilder.core.integration import aiohttp_error_middleware
 from aiohttp_admin2 import setup_admin
+from aiohttp_admin2.views import DashboardView
+from aiohttp_admin2.views import Admin
+from aiohttp_admin2.views.aiohttp.views.template_view import TemplateView
+
 import aiohttp_jinja2
 import jinja2
 from pathlib import Path
 
+# path to the your template directory
+templates_directory = Path(__file__).parent / 'templates'
 
-template_directory = Path(__file__).parent / 'templates'
+class CustomDashboard(DashboardView):
+    template_name = 'my_custom_dashboard.html'
+    async def get_context(self, req):
+            return {
+                **await super().get_context(req=req),
+                "content": "My custom content"
+            }
+
+
+class CustomAdmin(Admin):
+    dashboard_class = CustomDashboard
+
+
+class FirstCustomView(TemplateView):
+    name = 'Template view'
 
 async def message_queue():
     #logger.info("Start message processing")
@@ -41,45 +61,25 @@ async def run_api():
 
     asyncio.create_task(message_queue())
 
-
-
     APP = web.Application()
-    
-    # aiohttp_jinja2.setup(
-    #     app=APP,
-    #     loader=jinja2.FileSystemLoader(str(template_directory)),
-    # )
-    
-    setup_admin(APP,middleware_list=[aiohttp_error_middleware])
+    # setup jinja2
+    aiohttp_jinja2.setup(
+        app=APP,
+        loader=jinja2.FileSystemLoader(str(templates_directory)),
+    )
+
+    setup_admin(APP, admin_class=CustomAdmin, middleware_list=[aiohttp_error_middleware], views=[FirstCustomView,])
 
     APP.router.add_post("/api/messages", messages)
     for routes in APP.router.routes():
         print(routes)
 
-    #web.run_app(APP, port=config.PORT)
-    #web.run_app(APP)
     runner = web.AppRunner(APP)
-    #logger.info("Webserver Initiated")
+
     await runner.setup()
     await web.TCPSite(runner, host="localhost", port=config.PORT).start()
-    #await site.start()
-
-    #await process_message()
 
     await asyncio.Event().wait()
-
-# async def main():
-    
-#     logger = server_logging.logging.getLogger('Server') 
-#     logger.addHandler(server_logging.file_handler)
-#     logger.info(f"Init Server")
-
-    
-
-#     tasks = []
-#     tasks.append(asyncio.create_task(run_api()))
-#     tasks.append(asyncio.create_task(message_queue()))
-#     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(run_api())
